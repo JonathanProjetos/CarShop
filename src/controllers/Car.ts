50ea3858e5e360ecdc6815c80869e30f5471d7fc
Jonathan Santos <jonathan01347@gmail.com>
2022-10-21 20:13:38 -0300

    feat(controller): implementado readOne de Car

--- src/controllers/Car.ts
+++ src/controllers/Car.ts
@@ -15,7 +15,11 @@ class CarController {
     return res.status(201).json(created);
   }
 
- 
+  public async readOne(req:Request, res:Response<ICar>) {
+    const { id } = req.params;
+    const findById = await this._service.readOne(id);
+    return res.status(200).json(findById);
+  }
 }
 
 export default CarController;
