10741cd761c8a0c9155470deaf02966a66b5482e
Jonathan Santos <jonathan01347@gmail.com>
2022-10-21 20:21:00 -0300

    feat(controller): implementado a update Car

--- src/controllers/Car.ts
+++ src/controllers/Car.ts
@@ -31,6 +31,13 @@ class CarController {
     const destroy = await this._service.delete(id);
     return res.status(200).json(destroy);
   }
+
+  public async update(req:Request, res:Response<ICar>) {
+    const { id } = req.params;
+    const { body } = req;
+    const updated = await this._service.update(id, body);
+    return res.status(200).json(updated);
+  }
 }
 
 export default CarController;
