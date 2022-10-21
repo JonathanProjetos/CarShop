629e862514163a942f0893d91a31731efedb5e40
Jonathan Santos <jonathan01347@gmail.com>
2022-10-21 19:30:46 -0300

    feat(service): implementado update de Car

--- src/services/Car.ts
+++ src/services/Car.ts
@@ -4,13 +4,13 @@ import { IModel } from '../interfaces/IModel';
 import { ErrorTypes } from '../errors/catalog';
 
 class CarServices implements IService<ICar> {
-  private _car:IModel<ICar>;
+  private _car: IModel<ICar>;
 
-  constructor(model:IModel<ICar>) {
+  constructor(model: IModel<ICar>) {
     this._car = model;
   }
 
-  public async created(obj:unknown):Promise<ICar> {
+  public async create(obj: unknown): Promise<ICar> {
     const verify = CarZodSchema.safeParse(obj);
 
     if (!verify.success) throw verify.error;
@@ -19,21 +19,33 @@ class CarServices implements IService<ICar> {
     return created;
   }
 
-  public async findAll():Promise<ICar[]> {
+  public async read(): Promise<ICar[]> {
     const findAll = await this._car.read();
     return findAll;
   }
 
-  public async findById(_id:string):Promise<ICar> {
+  public async readOne(_id: string): Promise<ICar> {
     const findById = await this._car.readOne(_id);
     if (!findById) throw new Error(ErrorTypes.InvalidMongoId);
     return findById;
   }
 
-  public async detroy(_id:string):Promise<ICar> {
+  public async delete(_id: string): Promise<ICar> {
     const del = await this._car.delete(_id);
     if (!del) throw new Error(ErrorTypes.InvalidMongoId);
     return del;
+  }
+
+  public async update(_id: string, obg: unknown): Promise<ICar> {
+    const verify = CarZodSchema.safeParse(obg);
+
+    if (!verify.success) throw verify.error;
+
+    const updated = await this._car.update(_id, verify.data);
+
+    if (!updated) throw new Error(ErrorTypes.EntityNotFound);
+
+    return updated;
   }
 }
 
