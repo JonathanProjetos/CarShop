e3ffe4ce5ed56862613721caccea3b2f81e1809b
Jonathan Santos <jonathan01347@gmail.com>
2022-10-24 10:32:42 -0300

    feat(Motocycle): implementado classe Motocycle

--- src/interfaces/IMotorcycle.ts
+++ src/interfaces/IMotorcycle.ts
@@ -1,10 +1,10 @@
 import { z } from 'zod';
 import { VehicleZodSchema } from './IVehicle';
 
-const MotocycleZodSchema = VehicleZodSchema.extend({
+const MotorcycleZodSchema = VehicleZodSchema.extend({
   category: z.enum(['Street', 'Custom', 'Trail']), 
   engineCapacity: z.number().int().gte(0).lte(2500),
 });
 
-export type IMothocycle = z.infer<typeof MotocycleZodSchema>;
-export { MotocycleZodSchema };    
+export type IMotorcycle = z.infer<typeof MotorcycleZodSchema>;
+export { MotorcycleZodSchema };    
---
+++ src/models/Motocycle.ts
@@ -0,0 +1,25 @@
+import { model as mongooseCreateModel, Schema } from 'mongoose';
+import { IMotorcycle } from '../interfaces/IMotorcycle';
+import MongoModel from './MongoModel';
+
+const motocycleMonogSchema = new Schema<IMotorcycle>({
+  status: Boolean,
+  year: Number,
+  buyValue: Number,
+  color: String,
+  model: String,
+  category: {
+    Street: String,
+    Custom: String,
+    Trail: String,
+  },
+  engineCapacity: Number,
+}, { versionKey: false });
+
+class Motocycle extends MongoModel<IMotorcycle> {
+  constructor(model = mongooseCreateModel('Motocycle', motocycleMonogSchema)) {
+    super(model);
+  }
+}
+
+export default Motocycle;
