6268b652666101304c01f3c554b43e8bd7b4bfc0
Jonathan Santos <jonathan01347@gmail.com>
2022-10-24 12:34:15 -0300

    feat(mock): implementado mocks para motorcycles

---
+++ src/tests/mocks/IMotocycle.ts
@@ -0,0 +1,41 @@
+import { IMotorcycle } from '../../interfaces/IMotorcycle'
+
+const motocycleMock:IMotorcycle = {
+  model: 'Suzuki',
+  year: 2020,
+  color: 'red',
+  buyValue: 3500000,
+  category: 'Street',
+  engineCapacity: 1250
+}
+
+const motocycleMockId:IMotorcycle & { _id:string } = {
+  _id: '6356abd6f623c5b8c8a673d3',
+  model: 'Titan',
+  year: 2020,
+  color: 'red',
+  buyValue: 3500,
+  category: 'Street',
+  engineCapacity: 125
+}
+
+const motocycleMockAll:IMotorcycle[] = [
+  {
+    model: 'Titan',
+    year: 2020,
+    color: 'red',
+    buyValue: 3500000,
+    category: 'Street',
+    engineCapacity: 140
+  },
+  {
+    model: 'Kawasaki Ninja H2R',
+    year: 2022,
+    color: 'black',
+    buyValue: 28000,
+    category: 'Street',
+    engineCapacity: 950
+  },
+]
+ 
+export { IMotorcycle, motocycleMockAll, motocycleMockId };
