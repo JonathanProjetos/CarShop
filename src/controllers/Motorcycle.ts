ea92b05fa53e274a571527987a743d00cf5aba88
Jonathan Santos <jonathan01347@gmail.com>
2022-10-24 11:17:29 -0300

    feat(controller): implementado create motorcycle

---
+++ src/controllers/Motorcycle.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+import { ICar } from '../interfaces/ICar';
+import { IService } from '../interfaces/IService';
+
+class CarController {
+  private _service: IService<ICar>;
+  constructor(service: IService<ICar>) {
+    this._service = service;
+  }
+
+  public async create(req:Request, res:Response<ICar>) {
+    const { body } = req;
+    const created = await this._service.create(body);
+    return res.status(201).json(created);
+  }
+}
+
+export default CarController;
